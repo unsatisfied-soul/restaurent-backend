7228e626c830fe3ce123783cbf41848cf87b9b1a
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-02-22 00:44:41 +0600

    loginpost

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run(){
                 // const token = jwt.sign({
                 //     email: user.email
                 // }, process.env.JWT_SECRET)
-                return res.json({email: user.email, status: process.env.JWT_SECRET})
+                return res.json({email: user.email, status: process.env.JWT_SECREAT})
             }else{
                 return res.json({status: 'error'})
             }
